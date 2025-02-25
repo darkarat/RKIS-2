724e32ccc5ce51cabbff9694934a9aba3483f222
darkarat <dar.karat3011@gmail.com>
2025-02-25 21:13:09 +0300

    lesson 1: make task 1

--- Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
+++ Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LimitedSizeStack;
 
 public class LimitedSizeStack<T>
 {
+	private readonly int _size; //размер стека
+	private readonly LinkedList<T> _items = new LinkedList<T>(); // Двухсвязный список для хранения элементов
+	
+	// принимает размер стека
 	public LimitedSizeStack(int undoLimit)
 	{
+		if (undoLimit <= 0)
+            {
+                throw new ArgumentException("size must be greater than 0.", nameof(undoLimit));
+            }
+		_size = undoLimit;
 	}
 
+	// Метод для добавления элемента в стек
 	public void Push(T item)
 	{
-		throw new NotImplementedException();
+		if (_items.Count == _size)
+            {
+                _items.RemoveFirst(); // Если стек достиг макс размера, удаляет самый глубокий элемент (первый)
+            }
+            _items.AddLast(item); // Добавляем новый элемент в конец списка
 	}
 
+	// Метод для извлечения элемента из стека
 	public T Pop()
 	{
-		throw new NotImplementedException();
+		if (_items.Count == 0)
+		{
+			throw new InvalidOperationException("Stack is empty.");
+		}
+        T lastItem = _items.Last!.Value; // Получаем последний элемент
+        _items.RemoveLast(); // Удаляем его из списка
+        return lastItem;
 	}
 
-	public int Count => throw new NotImplementedException();
+	public int Count => _items.Count;
 }
