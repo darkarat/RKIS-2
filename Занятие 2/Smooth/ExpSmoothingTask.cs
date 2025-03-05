8f7ac0fbed8b3e31c73f4e09a2354787ecd79943
darkarat <dar.karat3011@gmail.com>
2025-03-05 22:47:51 +0300

    lesson 2: make task 1

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace yield;
@@ -5,8 +6,29 @@ namespace yield;
 public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-	{
-		//Fix me!
-		return data;
-	}
+    {
+        if (data == null)
+            throw new ArgumentNullException(nameof(data));
+        if (alpha < 0 || alpha > 1)
+            throw new ArgumentOutOfRangeException(nameof(alpha), "Alpha must be in the range [0, 1].");
+
+        double? smoothedValue = null;
+        bool isFirstElement = true;
+
+        foreach (var point in data)
+        {
+            if (isFirstElement)
+            {
+                // Первый элемент не сглаживается
+                smoothedValue = point.OriginalY;
+                isFirstElement = false;
+            }
+            else
+            {
+                smoothedValue = alpha * point.OriginalY + (1 - alpha) * smoothedValue.Value;
+            }
+
+            yield return point.WithExpSmoothedY(smoothedValue.Value);
+        }
+    }
 }
