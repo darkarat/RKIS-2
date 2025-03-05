776e42eb6af5e8d0b04f3c77ce8f8812f16038ed
darkarat <dar.karat3011@gmail.com>
2025-03-05 23:13:58 +0300

    lesson 2: make task 2

--- Занятие 2/Smooth/MovingAverageTask.cs
+++ Занятие 2/Smooth/MovingAverageTask.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace yield;
@@ -5,8 +6,28 @@ namespace yield;
 public static class MovingAverageTask
 {
 	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
-	{
-		//Fix me!
-		return data;
-	}
+    {
+        if (data == null)
+            throw new ArgumentNullException(nameof(data));
+        if (windowWidth <= 0)
+            throw new ArgumentOutOfRangeException(nameof(windowWidth), "Window width must be > 0");
+
+        var queue = new Queue<double>(); // Очередь для хранения элементов текущего окна
+        double sum = 0; // Сумма эл в текущем окне
+
+        foreach (var point in data)
+        {
+            // Добавляем текущее значение в очередь и к сумме
+            queue.Enqueue(point.OriginalY);
+            sum += point.OriginalY;
+
+            // Если окно превысило заданную ширину удаляем самый старый эл
+            if (queue.Count > windowWidth)
+                sum -= queue.Dequeue();
+
+            // Вычисляем сред значение для текущего окна
+            double average = sum / Math.Min(queue.Count, windowWidth);
+            yield return point.WithAvgSmoothedY(average);
+        }
+    }
 }
