06d605c2377b33f26d3a6efb5b349d568a977565
darkarat <dar.karat3011@gmail.com>
2025-04-17 17:56:40 +0300

    lesson 6: make task 1 && task 2

--- Занятие 6/Bank/BankAccount.cs
+++ Занятие 6/Bank/BankAccount.cs
@@ -3,23 +3,33 @@ using System.Threading.Tasks;
 
 namespace Bank
 {
-	public class BankAccount
-	{
-		public Guid Id { get; } = Guid.NewGuid();
+    public class BankAccount
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        private decimal _balance;
+        
+        public decimal GetBalance() => _balance;
 
-		public decimal GetBalance()
-		{
-			throw new NotImplementedException();
-		}
+        public async Task DepositAsync(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Amount must be positive", nameof(amount));
+            
+            await Task.Delay(100);
+            _balance += amount;
+        }
 
-		public async Task DepositAsync(decimal amount)
-		{
-			throw new NotImplementedException();
-		}
-
-		public async Task WithdrawAsync(decimal amount)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public async Task WithdrawAsync(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Amount must be positive", nameof(amount));
+            
+            await Task.Delay(100);
+            
+            if (_balance < amount)
+                throw new InvalidOperationException("Insufficient funds");
+            
+            _balance -= amount;
+        }
+    }
 }
--- Занятие 6/Bank/BankServer.cs
+++ Занятие 6/Bank/BankServer.cs
@@ -1,27 +1,47 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Bank
 {
-	public class BankServer
-	{
-		private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
+    public class BankServer
+    {
+        private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
 
-		public async Task<Guid> CreateAccount(decimal initialBalance = 0)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<Guid> CreateAccount(decimal initialBalance = 0)
+        {
+            if (initialBalance < 0)
+                throw new ArgumentException("Initial balance cannot be negative");
 
-		public async Task PerformTransactionAsync(Guid fromAccountId, Guid toAccountId, decimal amount)
-		{
-			throw new NotImplementedException();
-		}
+            var account = new BankAccount();
+            if (initialBalance > 0) 
+                await account.DepositAsync(initialBalance);
+                
+            _accounts[account.Id] = account;
+            return account.Id;
+        }
 
-		public async Task<decimal> GetAccountBalanceAsync(Guid accountId)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public async Task PerformTransactionAsync(Guid fromId, Guid toId, decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Amount must be positive", nameof(amount));
+
+            if (!_accounts.TryGetValue(fromId, out var from) || !_accounts.TryGetValue(toId, out var to))
+                throw new KeyNotFoundException("Account not found");
+
+            if (from.GetBalance() < amount)
+                throw new InvalidOperationException("Insufficient funds");
+
+            await from.WithdrawAsync(amount);
+            await to.DepositAsync(amount);
+        }
+
+        public Task<decimal> GetAccountBalanceAsync(Guid accountId)
+        {
+            if (!_accounts.TryGetValue(accountId, out var account))
+                throw new KeyNotFoundException("Account not found");
+
+            return Task.FromResult(account.GetBalance());
+        }
+    }
 }
